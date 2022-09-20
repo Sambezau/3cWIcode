a9220dc73fe1c29a16b6308a64eec71fc1c49764
Samuel <samuel.meusburger@student.htldornbirn.at>
2022-09-20 16:07:52 +0200

    main java file

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void  main(String[] args) {
+        System.out.println("Hallo");
+
+        int number = 7;
+        String name = "Andi";
+
+        System.out.println(name);
+        System.out.println(number);
+    }
+    //strg blank
+}
