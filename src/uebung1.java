e8a66e3d47549483db8af333978ab1f2b66c4399
Samuel <samuel.meusburger@student.htldornbirn.at>
2022-09-28 10:51:10 +0200

    uebung1 and uebung2 finished

--- src/uebung1.java
+++ src/uebung1.java
@@ -1,2 +1,27 @@
+import java.util.Random;
+
 public class uebung1 {
+    public static void main(String[] args) {
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        // Gib die Zufallszahl aus
+
+        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+        // Wenn die Zahl größer als 50 ist gib aus "Large"
+
+        System.out.println(randomNumber);
+        if (randomNumber<20){
+            System.out.println("Mni");
+        }
+        else if ((randomNumber>=20) && (randomNumber<=50)) {
+            System.out.println("Medium");
+        }
+        else {
+            System.out.println("Large");
+        }
+
+    }
 }
---
+++ src/uebung2.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class uebung2 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        Random random1 = new Random();
+        int randomNumber1 = random.nextInt(100);
+
+        System.out.println(randomNumber);
+        System.out.println(randomNumber1);
+
+
+
+
+        if ((randomNumber<randomNumber1) && (randomNumber>50)){
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        }
+        else if ((randomNumber<30) || (randomNumber1<30)) {
+            System.out.println("Eine der beiden ist kleiner als 30");
+        }
+        else if ((randomNumber<50) && (randomNumber1!=50)){
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+        }
+        else {
+            System.out.println();
+        }
+
+
+    }
+}
